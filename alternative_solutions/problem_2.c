5f12d8845af1a1bf83b40393412afa36e4101634
muhammad iril <iril.rf@gmail.com>
2020-04-11 09:12:40 +0700

    solved problem 2

---
+++ alternative_solutions/problem_2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+
+    int limit = 4000000;
+    int sum = 0;
+    int a = 1;
+    int b = 1;
+    int c = a + b;
+
+    while (c < limit) {
+        sum += c;
+        a = b + c;
+        b = c + a;
+        c = a + b;
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
---
+++ my_solutions/problem_2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int fib(int n) {
+    if (n <= 1)
+        return n;
+    return fib(n-1) + fib(n-2);
+}
+
+int main() {
+
+    int counter = 0;
+    int x = 0;
+    int result = 0;
+
+    while(x <= 4000000) {
+        x = fib(counter);
+        if (x % 2 == 0) 
+            result += x;
+        counter++;
+    }
+
+    printf("%d\n", result);
+
+    return 0;
+}
